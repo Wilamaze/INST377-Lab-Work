56eb97ef61a841e62d4f5e331f5123dedc7ca387
Wilamaze <orlando.aguilar@live.com>
2023-04-13 22:08:33 -0400

    Lab 8 Final Commit

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -1,5 +1,4 @@
-
-function getRandomIntInclusive(min, max) {
+  function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.ceil(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -32,22 +31,52 @@ function getRandomIntInclusive(min, max) {
       const index = getRandomIntInclusive(0, list.length -1);
       return list[index]
     }) 
-    }
+  }
   
+  function initMap(){
+    // 38.9072° N, 77.0369° W
+    const carto = L.map('map').setView([38.98, -76.93], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(carto);
+    return carto;
+  }
+
+  function markerPlace(array, map) {
+    console.log('array for markers', array);
+
+    map.eachLayer((layer) => {
+      if (layer instanceof L.Marker) {
+        layer.remove();
+      }
+    });
+
+    array.forEach((item) => {
+      console.log('markerPlace', item);
+      const {coordinates} = item.geocoded_column_1;
+
+      L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    })
+
+  }
   
   async function mainEvent() { // the async keyword means we can make API requests
     const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
     const loadDataButton = document.querySelector('#data_load');
+    const clearDataButton = document.querySelector('#data_clear');
     const generateListButton = document.querySelector('#generate');
     const textField = document.querySelector('#resto');
   
     const loadAnimation = document.querySelector('#data_load_animation');
     loadAnimation.style.display = 'none';
     generateListButton.classList.add('hidden');
+
+    const carto = initMap();
   
     const storedData = localStorage.getItem('storedData');
-    const parsedData = JSON.parse(storedData);
-    if (parsedData.length > 0) {
+    let parsedData = JSON.parse(storedData);
+    if (parsedData?.length > 0) {
       generateListButton.classList.remove('hidden');
   }
 
@@ -61,11 +90,14 @@ function getRandomIntInclusive(min, max) {
       const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
   
       const storedList = await results.json();
-      localStorage.setItem('storedData', JSON.stringify(storedList))
+      localStorage.setItem('storedData', JSON.stringify(storedList));
+      parsedData = storedList;
 
-
+      if (storedList?.length > 0) {
+        generateListButton.classList.remove('hidden');
+    }
       loadAnimation.style.display = 'none';
-      console.table(storedList); 
+      // console.table(storedList); 
     });
   
     generateListButton.addEventListener('click', (event) => {
@@ -73,20 +105,25 @@ function getRandomIntInclusive(min, max) {
       currentList = cutRestaurantList(parsedData);
       console.log(currentList);
       injectHTML(currentList);
+      markerPlace(currentList, carto);
     })
 
     textField.addEventListener('input', (event) => {
         console.log('input', event.target.value);
         const newList = filterList(currentList, event.target.value);
-        console.log(newList)
-        injectHTML(newList)
+        console.log(newList);
+        injectHTML(newList);
+        markerPlace(newList, carto);
+    })
+
+    clearDataButton.addEventListener('click', (event) => {
+      console.log('clear browser data');
+      localStorage.clear();
+      console.log('localStorage Check', localStorage.getItem("storedData"));
     })
   }
   
   
-  /*
-    This adds an event listener that fires our main event only once our page elements have loaded
-    The use of the async keyword means we can "await" events before continuing in our scripts
-    In this case, we load some data when the form has submitted
-  */
+
+
   document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
